3ac2d234d72a9bb1bf423ae510848db92c1ab7e7
churim <manas.churi@cdk.com>
2019-07-16 15:50:30 +0530

    Remove Duplicate Characters From Strings

--- src/main/java/algorithms/string/RemoveDuplicateCharactersFromStrings.java
+++ src/main/java/algorithms/string/RemoveDuplicateCharactersFromStrings.java
@@ -10,9 +10,25 @@ Output:
 geksfor
 */
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class RemoveDuplicateCharactersFromStrings {
 
     public static void main(String[] args) {
 
+        final String input = "geeksforgeeks";
+        StringBuffer output = new StringBuffer();
+        char[] inputChars = input.toCharArray();
+        List chars = new ArrayList();
+
+        for (int i = 0; i < inputChars.length; i++) {
+            if (! chars.contains(inputChars[i])) {
+                output.append(inputChars[i]);
+                chars.add(inputChars[i]);
+            }
+        }
+
+        System.out.println(output.toString());
     }
 }
